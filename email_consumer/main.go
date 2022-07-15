8ec17a45c57e4cd47b8d4a44fbe6aea1241a8bfe
cvsgbhn <kbh123456g@gmail.com>
2022-07-15 20:08:52 +0300

    tried email sending; failed

--- email_consumer/main.go
+++ email_consumer/main.go
@@ -3,9 +3,34 @@ package main
 import (
 	"fmt"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+	"net/smtp"
 )
 
-func sendEmails() {}
+func sendEmails(body string) {
+	from := ""
+	password := ""
+
+	toEmailAddress := ""
+	to := []string{toEmailAddress}
+
+	host := "smtp.yandex.com"
+	port := "465"
+	address := host + ":" + port
+
+	subject := "Subject: This is the subject of the mail\n"
+	message := []byte(subject + body)
+
+	auth := smtp.PlainAuth("", from, password, host)
+
+	fmt.Println("AUTH: ", auth)
+
+	err := smtp.SendMail(address, auth, from, to, message)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("SENT")
+}
 
 func consumer() {
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
@@ -18,12 +43,13 @@ func consumer() {
 		panic(err)
 	}
 
-	c.SubscribeTopics([]string{"emails", "^aRegex.*[Ee]mails"}, nil)
+	c.SubscribeTopics([]string{"emails"}, nil) // "^aRegex.*[Ee]mails"
 
 	for {
 		msg, err := c.ReadMessage(-1)
 		if err == nil {
 			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+			go sendEmails(string(msg.Value))
 		} else {
 			// The client will automatically try to recover from all errors.
 			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
